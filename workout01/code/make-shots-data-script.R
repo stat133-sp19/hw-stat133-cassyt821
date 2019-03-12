95c571ccd31fae7b69f89a1a6c776b4eccaa9a4c
Cassandra Tang <ctang@pacbell.net>
2019-03-11 23:31:13 -0700

    First commit of R Script

---
+++ workout01/code/make-shots-data-script.R
@@ -0,0 +1,13 @@
+########################################################################
+##Title: Creating Shots csv File
+##Description: Who knows. Read and Find out
+##Inputs: IDK we'll figure it out when we code it
+##Outputs: Hopefully I remember to fix this header before I turn it in.
+########################################################################
+
+curry <- read.csv("../data/stephen-curry.csv", stringsAsFactors = FALSE)
+iguodala <- read.csv("../data/andre-iguodala.csv", stringsAsFactors = FALSE)
+green <- read.csv("../data/draymond-green.csv", stringsAsFactors = FALSE)
+durant <- read.csv("../data/kevin-durant.csv", stringsAsFactors = FALSE)
+thompson <- read.csv("../data/klay-thompson.csv", stringsAsFactors = FALSE)
+
