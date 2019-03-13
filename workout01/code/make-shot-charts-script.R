fcda4718a9c563660c419e92a29d42d84080146a
Cassandra Tang <ctang@pacbell.net>
2019-03-12 20:51:54 -0700

    completed up to halfway through step 4.

---
+++ workout01/code/make-shot-charts-script.R
@@ -0,0 +1,63 @@
+########################################################################
+##Title: Make Shot Charts Script
+##Description: Script that makes shot charts.
+##Inputs: IDK We'll See
+##Outputs: Hopefully shot charts
+########################################################################
+
+setwd("~/Desktop/Stats 133/statsgit/workout01/code")
+
+library(dplyr)
+library(ggplot2)
+library(jpeg)
+library(grid)
+
+curry <- read.csv("../data/stephen-curry.csv", stringsAsFactors = FALSE)
+iguodala <- read.csv("../data/andre-iguodala.csv", stringsAsFactors = FALSE)
+green <- read.csv("../data/draymond-green.csv", stringsAsFactors = FALSE)
+durant <- read.csv("../data/kevin-durant.csv", stringsAsFactors = FALSE)
+thompson <- read.csv("../data/klay-thompson.csv", stringsAsFactors = FALSE)
+
+court_file <- "../images/nba-court.jpg"
+court_image <- rasterGrob(readJPEG(court_file), width = unit(1, "npc"), height = unit(1, "npc"))
+
+curry_scatter <- ggplot(data = curry) + 
+  annotation_custom(court_image, -250, 250, -50, 420) +
+  geom_point(aes(x = x, y = y, color = shot_made_flag)) + 
+  ylim(-50, 420) +
+  ggtitle('Shot Chart: Stephen Curry (2016 Season)') + 
+  theme_minimal()
+ggsave("../images/stephen-curry-shot-chart.pdf", plot = curry_scatter, width = 6.5, height = 5, units = "in")
+
+iguodala_scatter <- ggplot(data = iguodala) +
+  annotation_custom(court_image, -250, 250, -50, 420) +
+  geom_point(aes(x = x, y = y, color = shot_made_flag)) + 
+  ylim(-50, 420) +
+  ggtitle('Shot Chart: Andre Iguodala (2016 Season)') + 
+  theme_minimal()
+ggsave("../images/andre-iguodala-shot-chart.pdf", plot = iguodala_scatter, width = 6.5, height = 5, units = "in")
+
+green_scatter <- ggplot(data = green) +
+  annotation_custom(court_image, -250, 250, -50, 420) +
+  geom_point(aes(x = x, y = y, color = shot_made_flag)) +
+  ylim(-50, 420) +
+  ggtitle('Shot Chart: Draymond Green (2016 Season)') + 
+  theme_minimal()
+ggsave("../images/draymond-green-shot-chart.pdf", plot = green_scatter, width = 6.5, height = 5, units = "in")
+
+durant_scatter <- ggplot(data = durant) +
+  annotation_custom(court_image, -250, 250, -50, 420) +
+  geom_point(aes(x = x, y = y, color = shot_made_flag)) +
+  ylim(-50, 420) +
+  ggtitle('Shot Chart: Kevin Durant (2016 Season)') + 
+  theme_minimal()
+ggsave("../images/kevin-durant-shot-chart.pdf", plot = durant_scatter, width = 6.5, height = 5, units = "in")
+
+thompson_scatter <- ggplot(data = thompson) +
+  annotation_custom(court_image, -250, 250, -50, 420) +
+  geom_point(aes(x = x, y = y, color = shot_made_flag)) +
+  ylim(-50, 420) +
+  ggtitle('Shot Chart: Klay Thompson (2016 Season)') + 
+  theme_minimal()
+ggsave("../images/klay-thompson-shot-chart.pdf", plot = thompson_scatter, width = 6.5, height = 5, units = "in")
+
--- workout01/code/make-shots-data-script.R
+++ workout01/code/make-shots-data-script.R
@@ -1,9 +1,14 @@
 ########################################################################
 ##Title: Creating Shots csv File
-##Description: Who knows. Read and Find out
-##Inputs: IDK we'll figure it out when we code it
-##Outputs: Hopefully I remember to fix this header before I turn it in.
+##Description: Creates a amalgamated csv file for all players
+##Inputs: Requires no inputs; takes csv files from data directory
+##Outputs: Summary files for each player, summary file for master csv,
+##        and master csv.
 ########################################################################
+setwd("~/Desktop/Stats 133/statsgit/workout01/code")
+
+library(dplyr)
+library(ggplot2)
 
 curry <- read.csv("../data/stephen-curry.csv", stringsAsFactors = FALSE)
 iguodala <- read.csv("../data/andre-iguodala.csv", stringsAsFactors = FALSE)
@@ -11,3 +16,47 @@ green <- read.csv("../data/draymond-green.csv", stringsAsFactors = FALSE)
 durant <- read.csv("../data/kevin-durant.csv", stringsAsFactors = FALSE)
 thompson <- read.csv("../data/klay-thompson.csv", stringsAsFactors = FALSE)
 
+curry <- mutate(curry, name = "Stephen Curry")
+durant <- mutate(durant, name = "Kevin Durant")
+green <- mutate(green, name = "Draymond Green")
+iguodala <- mutate(iguodala, name = "Andre Iguodala")
+thompson <- mutate(thompson, name = "Klay Thompson")
+
+curry$shot_made_flag[curry$shot_made_flag == 'n'] = "shot_no"
+curry$shot_made_flag[curry$shot_made_flag == 'y'] = "shot_yes"
+durant$shot_made_flag[durant$shot_made_flag == 'n'] = "shot_no"
+durant$shot_made_flag[durant$shot_made_flag == 'y'] = "shot_yes"
+green$shot_made_flag[green$shot_made_flag == 'n'] = "shot_no"
+green$shot_made_flag[green$shot_made_flag == 'y'] = "shot_yes"
+iguodala$shot_made_flag[iguodala$shot_made_flag == 'n'] = "shot_no"
+iguodala$shot_made_flag[iguodala$shot_made_flag == 'y'] = "shot_yes"
+thompson$shot_made_flag[thompson$shot_made_flag == 'n'] = "shot_no"
+thompson$shot_made_flag[thompson$shot_made_flag == 'y'] = "shot_yes"
+
+curry <- mutate(curry, minute = period * 12 - (12 - minutes_remaining))
+durant <- mutate(durant, minute = period * 12 - (12 - minutes_remaining))
+green <- mutate(green, minute = period * 12 - (12 - minutes_remaining))
+iguodala <- mutate(iguodala, minute = period * 12 - (12 - minutes_remaining))
+thompson <- mutate(thompson, minute = period * 12 - (12 - minutes_remaining))
+
+sink()
+summary(curry)
+sink(file = "../output/stephen-curry-summary.txt")
+sink()
+summary(durant)
+sink(file = "../output/kevin-durant-summary.txt")
+sink()
+summary(green)
+sink(file = "../output/draymond-green-summary.txt")
+sink()
+summary(iguodala)
+sink(file = "../output/andre-iguodala-summary.txt")
+sink()
+summary(thompson)
+sink(file = "../output/klay-thompson-summary.txt")
+
+player_data <- rbind(curry, durant, green, iguodala, thompson)
+write.csv(player_data, file = "../data/shots-data.csv")
+sink()
+summary(player_data)
+sink(file = "../output/shots-data-summary.txt")
